e3c6869abb3760abbdc46ba6872f1b8f6153edad
Youssef Samaan <ysamaan2001@gmail.com>
2025-02-11 21:21:27 -0500

    changed the path to the pdf

--- src/data/ResumesItems.ts
+++ src/data/ResumesItems.ts
@@ -5,12 +5,12 @@ export const resumes: ResumeItem[] = [
         name: "Machine Learning and Research Resume",
         description: "This resume includes a research section. It applies to Research related roles",
         lastModified: "2024-03-15",
-        link: "/Resumes/Youssef_Samaan_Resume_ML_Research.pdf",
+        link: "/Personal-website/Resumes/Youssef_Samaan_Resume_ML_Research.pdf",
     },
     {
         name: "Software Engineering Resume",
         description: "This resume does not have a research section. it applies to also Data engineering and data science",
         lastModified: "2024-03-10",
-        link: "/Resumes/Youssef_Samaan_Resume.pdf",
+        link: "/Personal-website/Resumes/Youssef_Samaan_Resume.pdf",
     },
 ];
