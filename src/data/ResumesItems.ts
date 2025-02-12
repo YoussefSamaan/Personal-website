36dc87aa36cc4e8fa1dabe9d28e21dcd58cccbec
Youssef Samaan <ysamaan2001@gmail.com>
2025-02-11 19:49:37 -0500

    checking the links

--- src/data/ResumesItems.ts
+++ src/data/ResumesItems.ts
@@ -2,17 +2,15 @@ import {ResumeItem} from "../types.ts";
 
 export const resumes: ResumeItem[] = [
     {
-        id: 1,
         name: "Machine Learning and Research Resume",
         description: "This resume includes a research section. It applies to Research related roles",
         lastModified: "2024-03-15",
-        link: "./Resumes/Youssef_Samaan_Resume_ML_Research.pdf",
+        link: "../../Resumes/Youssef_Samaan_Resume.pdf",
     },
     {
-        id: 2,
         name: "Software Engineering Resume",
         description: "This resume does not have a research section. it applies to also Data engineering and data science",
         lastModified: "2024-03-10",
-        link: "./Resumes/Youssef_Samaan_Resume.pdf",
+        link: "../Resumes/Youssef_Samaan_Resume.pdf",
     },
 ];
